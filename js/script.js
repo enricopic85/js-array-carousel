957f30821e108057ab05a1bd7801fac35011a12d
enricopic85 <enricodev85@gmail.com>
2021-12-14 17:03:04 +0100

    pulizia codice

--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ const arrayPar=[
     "Ah che bello domani mi sveglio e mi taglio le vene, molto meglio per tutti, io mi levo il pensiero e anche all'INPS, gli conviene... Nella tomba mi porto l'iphone e l'ipad, se vi serve qualcosa mandate un whatsapp, giocherò a candy crush, se ogni tanto vi va, è ben accetta una vita",
     "L'America ci guarda, non proprio con affetto, apparantemente placida ci osserva, ma infondo io sospetto, che l'America ha paura, altrimenti non si spiega come faccia, a vedere in uno stato in miniatura, questa orribile minaccia",
     "Il conformista, è uno che di solito sta sempre dalla parte giusta e a tutte le risposte chiare dentro la sua testa. E se ha voglia di pensare, parla per sentito dire, da buon opportunista, si adegua senza farci caso, e vive nel suo paradiso."
-]
+];
 const arrayImg=[
     "url('https://www.repstatic.it/content/nazionale/img/2021/07/17/192948610-0e28f8f4-fca7-42c4-ae57-87818d8eac75.jpg')",
     "url('https://concertionline.com/wp-content/uploads/2018/01/Mirkoeilcane-Roma-05-01-18-7.jpg')",
     "url('https://www.insidemusic.it/wp-content/uploads/2017/06/Daniele-Silvestri_copertina.jpg')",
     "url('https://www.donnaglamour.it/wp-content/uploads/2019/06/IN_Giorgio_Gaber-1200x800.jpg')"
-]
+];
+//imposto il contatore a 0 per farlo iniziare dal primo elemento dell'array
 let currentIndex=0;
 
 const boxTitle=document.getElementById("titolo")
+//in questo modo collego la posizione 0 e visualizzo il primo dato contenuto
 boxTitle.innerHTML=arrayTitle[currentIndex];
 const boxPar=document.getElementById("testo");
 boxPar.innerHTML=arrayPar[currentIndex];
@@ -23,7 +25,8 @@ boxImg.style.backgroundImage=arrayImg[currentIndex];
 const buttonElement=document.querySelector(".fa-arrow-right");
 buttonElement.addEventListener('click',function(){
     currentIndex++;
-    if (currentIndex >= arrayTitle.length && currentIndex >= arrayPar.length && currentIndex >= arrayImg.length) {
+    //per creare il loop infinito
+    if (currentIndex >= arrayTitle.length) {
         currentIndex=0;
     }
     boxTitle.innerHTML=arrayTitle[currentIndex];
@@ -33,10 +36,9 @@ buttonElement.addEventListener('click',function(){
 const ButtonDown=document.querySelector(".fa-arrow-left");
 ButtonDown.addEventListener('click',function(){
     currentIndex--;
+    //per scorrere all'indietro
     if (currentIndex < 0) {
         currentIndex=arrayTitle.length-1;
-        currentIndex=arrayPar.length-1;
-        currentIndex=arrayImg.length-1;
         boxTitle.innerHTML=arrayTitle[currentIndex];
         boxPar.innerHTML=arrayPar[currentIndex];
         boxImg.style.backgroundImage=arrayImg[currentIndex];
